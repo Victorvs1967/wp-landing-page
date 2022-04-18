5153fd3484e3c6b583d788adceb15b7905114224
Victor <victorsmirnov@me.com>
2022-04-18 12:59:17 +0300

    add portfolio page

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 const mainHead = document.querySelector('.main__head'),
+      overflow = document.querySelector('.overflow'),
       menu = document.querySelector('.menu'),
       sandwich = menu.querySelector('.sandwich'),
       menuTop = document.querySelector('.menu__top'),
@@ -6,16 +7,24 @@ const mainHead = document.querySelector('.main__head'),
       loader = document.querySelector('.loader'),
       loaderInner = document.querySelector('.loader_inner'),
       titleWrapper = document.querySelector('.title-wrapper'),
+      portfolioLis = document.querySelectorAll('.portfolio li'),
+      containerEl = document.querySelector('.portfolio .container'),
+      portfolioItems = document.querySelectorAll('.portfolio__item'),
       parallax = document.querySelector('.parallax'), // parallax
-      parallaxBg = document.querySelector('.parallax-bg'); // parallax
+      parallaxBg = parallax.querySelector('.parallax-bg'); // parallax
+
+
+const mixer = mixitup(containerEl);
 
 mainHead.style.minHeight = `${window.screen.height}px`;
 
+// animate on scroll with AOS library
 AOS.init({
   duration: 1000,
   delay: 100,
 });
 
+// animated burger button w/o jQuery
 sandwich.addEventListener('click', () => {
   menuTop.classList.contains('active') ? fadeOut(menuTop, 400) : fadeIn(menuTop, 400);
   sandwich.classList.toggle('active');
@@ -26,6 +35,8 @@ sandwich.addEventListener('click', () => {
 });
 
 window.onload = () => {
+
+  // animated loader w/o jQuery
   document.body.classList.add('loaded_hiding');
   window.setTimeout(() => {
     document.body.classList.add('loaded');
@@ -41,21 +52,34 @@ window.onload = () => {
     for (let i = 0; i <= 1.0; i += 0.005) {
       thresholdSets.push(i);
     }
-    
     const callback = (entries, observer) => {
       const scrollTopPercent = window.pageYOffset / parallax.offsetHeight * 100;
       setParallaxItemsStyle(scrollTopPercent);
     };
-
-    const observer = new IntersectionObserver(callback, { threshold: thresholdSets });
-    
-    observer.observe(document.querySelector('section'));
-    
+    const observer = new IntersectionObserver(callback, { threshold: thresholdSets });   
+    observer.observe(document.querySelector('section'));    
     const setParallaxItemsStyle = scrollTopPercent => parallaxBg.parentElement.style.cssText = `transform: translate(0%, -${scrollTopPercent / 6}%);`;
   }
-
 };
 
+portfolioLis.forEach(li => li.addEventListener('click', () => {
+  portfolioLis.forEach(li => li.classList.remove('active'));
+  li.classList.add('active');
+}));
+
+portfolioItems.forEach(item => {
+  item.querySelector('a').addEventListener('click', event => {
+    fadeIn(overflow, 600);
+    const description = document.querySelector(event.target.href.substring(26));
+    description.classList.remove('hidden');
+    overflow.addEventListener('click', () => {
+      description.classList.add('hidden');
+      fadeOut(overflow, 600);
+    });
+  });
+});
+
+// animate menu w/o jQuiery
 menuTopItems.forEach(item => {
   item.addEventListener('click', () => {
     fadeOut(menuTop, 400);
@@ -64,6 +88,7 @@ menuTopItems.forEach(item => {
   });
 });
 
+// fadeIn/fadeOut effect w/o jQuiery
 const fadeIn = (el, timeout) => {
   el.style.opacity = 0;
   el.style.display = 'block';
